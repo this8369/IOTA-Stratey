aff701c31376f0a99da6e700c9962e7823bfa106
this8369 <this8369@hotmail.com>
2026-05-29 12:41:37 +0900

    chore: setup subdomain stratey.iotaseoul.site and adjust base path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig(({ command }) => ({
-  base: '/IOTA-Stratey/',
+  base: '/',
   plugins: [
     react(),
     tailwindcss(),
